a1aef12eb0af9e6453bf17b828bbb600b17d6f4c
Mirko Guerra <mirkoguerra93@gmail.com>
2020-10-20 17:19:00 +0200

    Arrotondamento
    Anche la parte relativa all'arrotondamento sembra funzionare correttamente. Tuttavia, immagino che ci sia un modo più snello, in termini di codice, per risolvere questo problema

--- js/script.js
+++ js/script.js
@@ -2,11 +2,21 @@ var distanza = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
 var anni = parseInt(prompt("Quanti anni hai?"));
 // i dati che mi fornisce l'utente
 var prezzoIntero = distanza * 0.21;
+var multiplier = 100;
+var prezzoIntero = Math.round(prezzoIntero * multiplier) / multiplier;
 // questa variabile rappresenta il prezzo del biglietto, senza tenere conto dell'eventuale sconto
 var scontoMinore = prezzoIntero * 0.2;
+var scontoMinore = Math.round(scontoMinore * multiplier) / multiplier;
 // questa variabile rappresenta lo sconto sul prezzo del biglietto se l'acquirente è minorenne
 var scontoAnziani = prezzoIntero * 0.4;
+var scontoAnziani = Math.round(scontoAnziani * multiplier) / multiplier;
 // questa variabile rappresenta lo sconto sul prezzo del biglietto se l'acquirente è anziano
+var prezzoFinaleMinore = (prezzoIntero - scontoMinore)
+var prezzoFinaleMinore = Math.round(prezzoFinaleMinore * multiplier) / multiplier;
+// questa variabile rappresenta il prezzo finale per i minori
+var prezzoFinaleAnziani = (prezzoIntero - scontoAnziani)
+var prezzoFinaleAnziani = Math.round(prezzoFinaleAnziani * multiplier) / multiplier;
+// questa variabile rappresenta il prezzo finale per gli over 65
 
 if (isNaN(distanza) || isNaN(anni)) {
   document.getElementById("km").innerHTML = "Si prega di inserire solo valori numerici";
@@ -21,12 +31,12 @@ if (isNaN(distanza) || isNaN(anni)) {
   // questa parte resta fissa a prescindere dall'età del cliente
     if (anni<18) {
       document.getElementById("discount").innerHTML = "Lo sconto applicato in base alla tua età è di: " + scontoMinore + " £";
-      document.getElementById("final-price").innerHTML = "Il prezzo finale del biglietto è: " + (prezzoIntero-scontoMinore) + " £";
+      document.getElementById("final-price").innerHTML = "Il prezzo finale del biglietto è: " + prezzoFinaleMinore + " £";
       // qui ho gestito la scontistica per un acquirente minorenne
     }
     else if (anni>65) {
       document.getElementById("discount").innerHTML = "Lo sconto applicato in base alla tua età è di: " + scontoAnziani + " £";
-      document.getElementById("final-price").innerHTML = "Il prezzo finale del biglietto è: " + (prezzoIntero-scontoAnziani) + " £";
+      document.getElementById("final-price").innerHTML = "Il prezzo finale del biglietto è: " + prezzoFinaleAnziani + " £";
       // qui ho gestito la scontistica per un acquirente over 65
     }
     else {
