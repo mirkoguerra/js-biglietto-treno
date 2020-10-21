67489ceecb9442560bc8b5e4c87ccd3fb88f350a
Mirko Guerra <mirkoguerra93@gmail.com>
2020-10-21 17:19:32 +0200

    Dichiarazione variabili

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-
+document.getElementById("genera").addEventListener("click", function(){
+  var name = document.getElementById("nome").value;
+  console.log(name);
+  var km = document.getElementById("km").value;
+  console.log(km);
+  var age = document.getElementById("eta").value;
+  console.log(age);
+});
